d03decc4bc391dc6454ad65188b8cd2cbfa1fc6d
qorwhddls134 <qorwhddls134@naver.com>
2026-01-27 12:10:06 +0900

    feat:코드생성

--- src/pages/PostCreatePage.jsx
+++ src/pages/PostCreatePage.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 
 function PostCreatePage() {
+  const [title, setTitle] = useState('');
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
   return (
     <div>
-      <h1>MainPage 입니다.</h1>
+      <p>To.</p>
+      <input 
+        type='text'
+        placeholder='받는 사람 이름을 입력해 주세요'
+        value={title}
+        onChange={handleChange}
+      />
+      <h1>배경화면을 선택해 주세요.</h1>
+      <p>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
     </div>
   );
 }
